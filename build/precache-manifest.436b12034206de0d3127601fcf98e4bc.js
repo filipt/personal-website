1c9f64ec76dd6a601d822e861fa958b9e9b1d014
filipt <filip@pointapi.com>
2020-06-14 02:38:58 -0700

    added untracked files

---
+++ build/precache-manifest.436b12034206de0d3127601fcf98e4bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e3960af15b8e41e6ab8004febc33b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "834e9607d71251f59a9e",
+    "url": "/static/css/2.be70c16c.chunk.css"
+  },
+  {
+    "revision": "f685c6423ad6d23dbd80",
+    "url": "/static/css/main.900cc791.chunk.css"
+  },
+  {
+    "revision": "834e9607d71251f59a9e",
+    "url": "/static/js/2.c40a87c9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c40a87c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f685c6423ad6d23dbd80",
+    "url": "/static/js/main.21ed7922.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "293d83e7fcad68ba451b9f052a468a46",
+    "url": "/static/media/it-is-i-the-fil.293d83e7.png"
+  }
+]);
